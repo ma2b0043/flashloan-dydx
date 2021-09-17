f65cea925c7d5f1cd098a9979d74d388e0f7bef9
ma2b0043 <anas.azam40@gmail.com>
2021-09-17 23:43:41 +0500

    fix bugs - third attempt

--- test/util.js
+++ test/util.js
@@ -1,3 +1,5 @@
+const BN = require("bn.js");
+
 function sendEther(web3, from, to, amount) {
   return web3.eth.sendTransaction({
     from,
@@ -8,7 +10,36 @@ function sendEther(web3, from, to, amount) {
 
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
+function cast(x) {
+  if (x instanceof BN) {
+    return x;
+  }
+  return new BN(x);
+}
+
+function eq(x, y) {
+  x = cast(x);
+  y = cast(y);
+  return x.eq(y);
+}
+
+function pow(x, y) {
+  x = cast(x);
+  y = cast(y);
+  return x.pow(y);
+}
+
+function frac(x, n, d) {
+  x = cast(x);
+  n = cast(n);
+  d = cast(d);
+  return x.mul(n).div(d);
+}
+
 module.exports = {
   sendEther,
   ZERO_ADDRESS,
+  eq,
+  pow,
+  frac,
 };
